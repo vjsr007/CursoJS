f32f66514424d58e3ac716af2609dd36313d433c
Vj <vjsanchezr@hotmail.com>
2021-07-15 20:56:54 -0500

    .

--- Dia02.1/main.js
+++ Dia02.1/main.js
@@ -1,10 +1,21 @@
 const init = () => {
     const root = document.getElementById("root");
+
     const hello = document.createElement("hello-world");
+    const hello2 = document.createElement("hello-world");
+    const hello3 = document.createElement("hello-world");
 
-    hello.setAttribute("message", "Hola soy un componente");
+    hello.setAttribute("message", "Hola soy un componente 1");
 
-    root.innerHTML = hello.outerHTML;
+    root.appendChild(hello);
+
+    hello2.setAttribute("message", "Hola soy un componente 2");
+
+    root.appendChild(hello2);
+
+    hello3.setAttribute("message", "Hola soy un componente 3");
+
+    root.appendChild(hello3);
 }
 
 document.addEventListener("DOMContentLoaded", init)
