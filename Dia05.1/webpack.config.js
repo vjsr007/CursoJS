fda46f30731a60b0609d258395780f5b0372d6c0
TH658FY <victor.sanchez@ey.com>
2021-08-02 13:32:33 -0500

    .

---
+++ Dia05.1/webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html",
+});
+
+module.exports = () => {
+    return {
+        mode: "development",
+        module: {
+          rules: [
+            {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              use: {
+                loader: "babel-loader",
+              },
+            },
+            {
+              test: /\.css$/,
+              use: ["style-loader", "css-loader"],
+            },
+          ],
+        },
+        plugins: [htmlPlugin],
+      };
+}
+
+
+
