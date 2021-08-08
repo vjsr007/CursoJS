907dbb3547658b7d1c43dc91bcc0f3d9e042abeb
Vj <vjsanchezr@hotmail.com>
2021-08-07 21:29:50 -0500

    .

--- Dia05/src/components/FilterNav/FilterNav.jsx
+++ Dia05/src/components/FilterNav/FilterNav.jsx
@@ -4,30 +4,18 @@ import CustomInput from '../CustomInput'
 import Dropdown from '../Dropdown'
 import Pager from '../Pager'
 import CustomButton from '../CustomButton'
-import CustomInputClass from '../CustomInputClass/CustomInput'
 
 import styles from './filterNav.scss'
 import { lans } from '../../constants/constants'
 
-const FilterNav = () => {
-  const showConsole = ev => {
-    console.log('text', ev.target.value)
-  }
-
-  const showConsole2 = ev => {
-    console.log('text2', ev.target.value)
-  }
-
-  return (
-    <div className={styles.component}>
-      <CustomInput handleOnChange={showConsole} />
-      <CustomInput handleOnChange={showConsole2} />
-      <CustomInputClass />
-      <Dropdown options={lans} multiSelect />
-      <Pager />
-      <CustomButton />
-    </div>
-  )
-}
+const FilterNav = () => (
+  <div className={styles.component}>
+    <CustomInput />
+    <CustomInput />
+    <Dropdown options={lans} multiSelect />
+    <Pager />
+    <CustomButton />
+  </div>
+)
 
 export default FilterNav
